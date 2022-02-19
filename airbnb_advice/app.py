f901daa7f21af77c161e8658d9995392f3a16f21
thomasgassin <thomas.gassin@gmail.com>
2022-02-19 10:25:12 +0100

    continuous deployment made easy by TG

--- airbnb_advice/app.py
+++ airbnb_advice/app.py
@@ -3,9 +3,18 @@ import streamlit as st
 st.markdown('''
 # AIR BNB ADVIC€
 ## Welcolme to our amazing app
+Richard, Nicolas, Christe
 
 ''')
 
 st.markdown('''
 #Thanks to provide the data in the inbox below so Artificial Intelligence can predict the TAXI FARE  : 
 ''')
+
+st.slider("number or rooms", 1,10,2)
+
+st.markdown(""" <style> .font {
+font-size:50px ; font-family: 'Cooper Black'; color: #FF9633;} 
+</style> """, unsafe_allow_html=True)
+
+st.markdown('<p class="font">Guess the object Names</p>', unsafe_allow_html=True)
