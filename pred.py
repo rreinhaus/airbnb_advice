a54524721ba7eed2fa28f6e4549e728963dbb726
Richard Reinhaus <r.reinhaus@gmail.com>
2022-03-08 20:38:22 +0100

    Pred file update

--- pred.py
+++ pred.py
@@ -2,39 +2,37 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
-from airbnb_advice.trainer import lines
-
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(lines)
-sequences = tokenizer.texts_to_sequences(lines)
-
-# Loading the deep learning model
-model = load_model('models_testdeep_model_best(1).h5')
+import pandas as pd
 
 # Input from the user for the prediction - THIS NEEDS TO BE CHANGED FROM THE STREAMLIT INPUT
-room_types = ['Entire Place', 'Private Room', 'Shared Room' ]
 
-def generate_text_seq(model, tokenizer, text_seq_length, seed_text, n_words):
+room_types = ['Balcony', 'Private Room', 'Shared Room' ]
+
+def generate_text_seq(input_text):
+  #lines = pd.read_csv('https://storage.googleapis.com/airbnbadvice/data/lines.csv')
+  lines = pd.read_csv('/home/rreinhaus/code/rreinhaus/airbnb_advice/airbnb_advice/data/lines.csv')
+  lines = lines['lines'].values.tolist()
+  # Tokenizing the input on the lines and achieving the sequences required for the model
+  tokenizer = Tokenizer()
+  tokenizer.fit_on_texts(lines)
+  sequences = tokenizer.texts_to_sequences(lines)
+  # Loading the deep learning model
+  model = load_model('models_testdeep_model_best(1).h5')
   text = []
-
-  for _ in range(n_words):
-    encoded = tokenizer.texts_to_sequences([seed_text])[0]
-    encoded = pad_sequences([encoded], maxlen=text_seq_length, truncating='pre')
-
+  for _ in range(8):
+    encoded = tokenizer.texts_to_sequences([input_text])[0]
+    encoded = pad_sequences([encoded], maxlen=6, truncating='pre')
     y_predict = np.argmax(model.predict(encoded), axis=-1)
-
     predicted_word = ''
     for word, index in tokenizer.word_index.items():
       if index == y_predict:
-        predicted_word = word 
+        predicted_word = word
         break
-    seed_text = seed_text + ' ' + predicted_word
+    input_text = input_text + ' ' + predicted_word
     text.append(predicted_word)
-
   return ' '.join(text)
 
-
 if __name__ == '__main__':
-    result = generate_text_seq(model, tokenizer, 6, seed_text=room_types[0], n_words=7)
-    title = room_types[0] + ' - ' + result
+    result = generate_text_seq('flat')
+    title = 'flat' + ' - ' + result
     print(title)
