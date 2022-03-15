5fe1921a854d586c4c04c57dbe34fa5fe541692a
Nicolas Frate <nicofragon@gmail.com>
2022-03-15 20:26:00 +0100

    final format chart page 1

--- airbnb_advice/app.py
+++ airbnb_advice/app.py
@@ -18,23 +18,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import folium
 from streamlit_folium import folium_static
-import plotly.express as px 
+import plotly.express as px
 import seaborn as sns
 import plotly.figure_factory as ff
 import time
 
-def density_map_hood(data, neighbourhood, lat_long_hood):
-    lat = lat_long_hood.loc[neighbourhood].latitude
-    lon = lat_long_hood.loc[neighbourhood].longitude 
-    m = folium.Map([lat, lon], zoom_start=14, tiles="CartoDB positron")
-    for index, row in data.iterrows():
-        folium.CircleMarker([row['latitude'], row['longitude']],
-                            radius=1,
-                            fill=True,
-                            opacity=0.7).add_to(m)
-    return m
+# def density_map_hood(data, neighbourhood, lat_long_hood):
+#     lat = lat_long_hood.loc[neighbourhood].latitude
+#     lon = lat_long_hood.loc[neighbourhood].longitude
+#     m = folium.Map([lat, lon], zoom_start=14, tiles="CartoDB positron")
+#     for index, row in data.iterrows():
+#         folium.CircleMarker([row['latitude'], row['longitude']],
+#                             radius=1,
+#                             fill=True,
+#                             opacity=0.7).add_to(m)
+#     return m
 
 #density plot given neighbourhood
+
 
 def density_map_hood(data, neighbourhood, lat_long_hood):
     lat = lat_long_hood.loc[neighbourhood].latitude
@@ -52,7 +53,7 @@ def csv_loader(X,neighbourhood):
     X = X[X['neighbourhood_cleansed'] == neighbourhood]
     lat_long_hood = X.groupby("neighbourhood_cleansed").mean()
     maps = density_map_hood(X, neighbourhood,lat_long_hood)
-    return folium_static(maps ) #X[["latitude","longitude"]])
+    return folium_static(maps) #X[["latitude","longitude"]])
 
 ###############
 # Rich Rating chart
@@ -67,7 +68,7 @@ def neighbourhood_reviews(neighbourhood):
     angles.pop()
     neighbourhood_rating = scores_rating[scores_rating['neighbourhood'] == neighbourhood]
     neighbourhood_rating = neighbourhood_rating.groupby('neighbourhood').median()
-    
+
     def add_to_star_neighbourhood(neighbourhood, color, label=None):
         values = neighbourhood_rating.loc[neighbourhood].tolist()
         values += values[:1]
@@ -79,7 +80,7 @@ def neighbourhood_reviews(neighbourhood):
             ax.plot(angles, values, color=color, linewidth=1, label=neighbourhood)
         ax.fill(angles, values, color=color, alpha=0.25)
 
-    ## Create plot object   
+    ## Create plot object
     fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
 
     ## Fix axis to star from top
@@ -206,8 +207,8 @@ def draw_plot(df, column):
     '''
     Returns a scatter plot
     '''
-    fig = px.histogram(df, x=column, nbins=10) 
-    
+    fig = px.histogram(df, x=column, nbins=10)
+
     return fig
 
 def keywords(neighbourhood):
@@ -228,7 +229,7 @@ def keywords(neighbourhood):
         keys_comments.append(df_comments['keywords'][key])
         keys_neighbourhoods.append(df_neighbourhood['keywords'][key])
         keys_description.append(df_description['keywords'][key])
-    
+
     comments = [item for item in keys_comments if item not in keys_neighbourhoods]
     neigh = [item for item in keys_neighbourhoods if item not in keys_comments]
     des = [item for item in keys_description if item not in keys_neighbourhoods]
@@ -238,9 +239,9 @@ def keywords(neighbourhood):
 
     for i, val in enumerate(comments):
         first_comments = comment_col[i].metric(str(i+1)+': comments', comments[i])
-        
+
     col1_n, col2_n, col3_n = st.columns(3)
-    
+
     neigh_col = [col1_n, col2_n, col3_n]
 
     for i, val in enumerate(neigh):
@@ -263,9 +264,9 @@ st.set_page_config(
             initial_sidebar_state="auto") # collapsed
 
 st.markdown('''
-# AIRBNB ADVICE 
+# AIRBNB ADVICE
 
-The aim of the application is to provide Airbnb hosts and potential hosts insights about their neighbourhood. 
+The aim of the application is to provide Airbnb hosts and potential hosts insights about their neighbourhood.
 
 We also try to help to optimize listings by providing price per night prediction and auto-generated titles and descriptions''')
 
@@ -282,7 +283,7 @@ nb_bedrooms = st.sidebar.slider("Number of rooms", 1,10,2) #the number of room
 nb_beds = st.sidebar.slider("Number of Beds",1,10,2) # the number of beds
 min_nights = st.sidebar.slider("Minimum nights that people have to stay", 1,7,1)
 accomodates = int(st.sidebar.number_input('How many guests can you accomodate?' , min_value=0, value=5, step=1 ))
-navi = st.sidebar.radio("Data Sections To Display", ('Neighbourhood Stats','Title & Describtions')) 
+navi = st.sidebar.radio("Data Sections To Display", ('Neighbourhood Stats','Title & Describtions'))
 
 # Getting pick up location as address and transforming to coordinates
 #neighbourhood = None
@@ -302,7 +303,7 @@ else:
         neighbourhood = location.raw['address']['city_district'].replace(
             'London Borough of ', '')
     elif 'quarter' in location.raw['address']:
-            neighbourhood = location.raw['address']['quarter'].replace(
+        neighbourhood = location.raw['address']['quarter'].replace(
         'London Borough of ','')
     elif 'suburb' in location.raw['address']:
         neighbourhood = location.raw['address']['suburb'].replace(
@@ -350,12 +351,12 @@ if navi == 'Title & Describtions':
         response_announce = requests.get(url).json()
         announce_predicted = response_announce['announce']
         st.text(words + ' - ' + announce_predicted)
-    
+
     st.markdown('---')
     st.markdown("""
             ### The top 3 Superhost comment & neighbourhood overview keywords
 
-            These are the keywords specific to your area that unsupervised machine learning model came up with ;) 
+            These are the keywords specific to your area that unsupervised machine learning model came up with ;)
         """)
     keywords(neighbourhood)
 
@@ -424,27 +425,50 @@ else:
 
     st.markdown('---')
 
-    # Density Map & Price Plots
-    chart1, chart2 = st.columns(2)
-    with chart1:
-        # Density Map
-        expander = st.expander("Density Map - More Info")
+    expander = st.expander("Density Map - More Info")
 
-        expander.markdown("""The Neighbourhood Density Map. 
+    expander.markdown("""The Neighbourhood Density Map.
 
-            The map presents each property within your neighbourhood and where it is located. 
+            The map presents each property within your neighbourhood and where it is located.
 
             The aim of the map is to see where the most properties are located in your specific area and how many there are.
 
             """)
+    c1, c2, c3 = st.columns([1, 3, 1])
+
+    with c2:
         csv_loader(data_maps,neighbourhood)
+
+    # Density Map & Price Plots
+    chart1, chart2 = st.columns(2)
+    with chart1:
+        occupancy_expand = st.expander("Occupancy Rates - More Info")
+        occupancy_expand.markdown("""Occupancy Rate Target Chart.
+
+                The chart showcases based on each listed property avaible and booked previous nights what is the aimed occupancy rate for each property.
+
+                P.S. The property occupancy rates are not guaranteed and this is not financial advise by any means.
+                """)
+        # Occupancy Plot
+
+        occup_plot = draw_plot(price_df[['occupancy_month']],
+                               'occupancy_month')
+        occup_plot.update_traces(marker_line_width=0.5,
+                                 marker_line_color="white")
+        occup_plot.update_layout(bargap=0.1)
+        st.plotly_chart(occup_plot, use_container_width=True)
+        # with st.container():
+        #     csv_loader(data_maps,neighbourhood)
+
+
+
     with chart2:
-        # Price Plot 
+        # Price Plot
         price_expand = st.expander("Property Price - More Info")
 
-        price_expand.markdown("""Property Price Distribution. 
+        price_expand.markdown("""Property Price Distribution.
 
-            The chart showcases how the properties are priced within your neighbourhood. 
+            The chart showcases how the properties are priced within your neighbourhood.
 
             Please take into consideration for better representation we have taken out the outliers.
 
@@ -452,30 +476,38 @@ else:
         Q1 = price_df.price.quantile(0.25).round(3)
         Q3 = price_df.price.quantile(0.75).round(3)
         IQR = Q3 - Q1
-        outlier = Q3 + 1.5*IQR 
+        outlier = Q3 + 1.5*IQR
         price_plot = px.histogram(price_df[price_df['price'] <= outlier], x='price', nbins=10)
-        st.write(price_plot)
-        
+        price_plot.update_traces(marker_line_width=0.5,
+                                 marker_line_color="white")
+        price_plot.update_layout(bargap=0.1)
+        st.plotly_chart(price_plot, use_container_width=True)
+        # st.write(price_plot)
+
     # Superhost & Revenue
     chart3, chart4 = st.columns(2)
     with chart3:
-        # Neighbourhood Superhost Plot 
+        # Neighbourhood Superhost Plot
         superhost_expand = st.expander("Superhost status - More Info")
 
-        superhost_expand.markdown("""Superhost status chart. 
+        superhost_expand.markdown("""Superhost status chart.
 
-            The chart showcases how many of the total neighbourhood property owners are superhosts. 
+            The chart showcases how many of the total neighbourhood property owners are superhosts.
             """)
         fig = px.histogram(data[data['neighbourhood_cleansed']== neighbourhood], x="host_is_superhost")
-        st.write(fig)
+        fig.update_traces(marker_line_width=0.5,
+                                 marker_line_color="white")
+        fig.update_layout(bargap=0.1)
+        st.plotly_chart(fig, use_container_width=True)
+        # st.write(fig)
 
     with chart4:
         # Revenue plot
         rev_expand = st.expander("Monthly revenues - More Info")
 
-        rev_expand.markdown("""Monhtly revenue chart. 
+        rev_expand.markdown("""Monhtly revenue chart.
 
-            The chart showcases based on each property occupancy rate, what is the distribution of airbnb property potential revenue targets. 
+            The chart showcases based on each property occupancy rate, what is the distribution of airbnb property potential revenue targets.
 
             P.S. The property revenues per month are not guaranteed and this is not financial advise by any means.
             """)
@@ -483,21 +515,29 @@ else:
         group_labels = ['pot_rev_month']
         rev_plot = px.histogram(price_df[['pot_rev_month']], x='pot_rev_month', nbins=10)
         #rev_plot = ff.create_distplot([price_df['pot_rev_month']],group_labels, bin_size=.10, curve_type='normal')
-        st.write(rev_plot)
+        rev_plot.update_traces(marker_line_width=0.5,
+                                 marker_line_color="white")
+        rev_plot.update_layout(bargap=0.1)
+        st.plotly_chart(rev_plot, use_container_width=True)
+        # st.write(rev_plot)
 
-    # Ratings & Occupancy
-    chart5, chart6 = st.columns(2)
-    with chart5:
-        # Occupancy Plot
-        occupancy_expand = st.expander("Occupancy Rates - More Info")
-        occupancy_expand.markdown("""Occupancy Rate Target Chart. 
+    # # Ratings & Occupancy
+    # chart5, chart6 = st.columns(2)
+    # with chart5:
+    #     # Occupancy Plot
+    #     occupancy_expand = st.expander("Occupancy Rates - More Info")
+    #     occupancy_expand.markdown("""Occupancy Rate Target Chart.
 
-                The chart showcases based on each listed property avaible and booked previous nights what is the aimed occupancy rate for each property. 
+    #             The chart showcases based on each listed property avaible and booked previous nights what is the aimed occupancy rate for each property.
 
-                P.S. The property occupancy rates are not guaranteed and this is not financial advise by any means.
-                """)
-        occup_plot = draw_plot(price_df[['occupancy_month']], 'occupancy_month')
-        st.write(occup_plot)
+    #             P.S. The property occupancy rates are not guaranteed and this is not financial advise by any means.
+    #             """)
+    #     occup_plot = draw_plot(price_df[['occupancy_month']], 'occupancy_month')
+    #     occup_plot.update_traces(marker_line_width=0.5,
+    #                              marker_line_color="white")
+    #     occup_plot.update_layout(bargap=0.1)
+    #     st.plotly_chart(occup_plot, use_container_width=True)
+    #     # st.write(occup_plot)
 
     # with chart6:
     #     # Neigbourhood Rating Plot
@@ -519,7 +559,7 @@ else:
 
                 price_df, neighbourhood)[2] * 365 * fare_predicted
 
-            st.text(f'''If you achieve the average occupancy rate of your area, 
+            st.text(f'''If you achieve the average occupancy rate of your area,
             your potential revenue is of £{round(monthly_revenue,2)} per month and
             £{round(yearly_revenue,2)} per year.''')
 
